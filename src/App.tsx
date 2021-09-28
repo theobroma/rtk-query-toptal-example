680958a920e4bd776ae23b8096cb26ddf2ef5e09
Alexandr Siryj <theobroma222@gmail.com>
2021-09-28 23:00:41 +0300

    store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { CssBaseline } from "@material-ui/core";
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { QueryParamProvider } from "use-query-params";
+import "./index.css";
+import FullscreenProgress from "./shared/components/FullscreenProgress/FullscreenProgress";
+import { persistor, store } from "./shared/redux/store";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={<FullscreenProgress />} persistor={persistor}>
+        <Router>
+          <QueryParamProvider ReactRouterRoute={Route}>
+            <CssBaseline />
+          </QueryParamProvider>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/shared/components/FullscreenProgress/FullscreenProgress.tsx
@@ -0,0 +1,19 @@
+import { Box, CircularProgress } from "@material-ui/core";
+import React from "react";
+
+const FullscreenProgress = () => {
+  return (
+    <Box
+      position="absolute"
+      width="100%"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      display="flex"
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default FullscreenProgress;
